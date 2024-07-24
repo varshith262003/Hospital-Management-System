cf3c1dcce2714619f9e56b80a645456e5130dcef
varshith262003 <150769333+varshith262003@users.noreply.github.com>
2024-07-24 20:47:02 +0530

    Update DeleteButton.jsx

--- Frontend/src/components/DeleteButton.jsx
+++ Frontend/src/components/DeleteButton.jsx
@@ -6,7 +6,7 @@ const DeleteButton = ({ id, onDelete }) => {
   const handleDelete = async () => {
     if (window.confirm('Are you sure you want to delete this hospital?')) {
       try {
-        await axios.delete(`http://localhost:5000/api/v1/hospitals/delete?id=${id}`);
+        await axios.delete(`https://hospitalserver-rose.vercel.app/api/v1/hospitals/delete?id=${id}`);
         onDelete(id); // Notify parent component
         alert('Hospital deleted successfully'); 
       } catch (error) {
