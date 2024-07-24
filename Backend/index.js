b620a9dc77189dbda752072e883cf03e477e3134
varshith262003 <150769333+varshith262003@users.noreply.github.com>
2024-07-24 21:03:35 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(cors(
     {
-        origin: 'http://localhost:5173',
+        origin: ['http://localhost:5173','https://hms-mern.netlify.app/'],
         credentials: true,
     }
 ));
